eaa1d9e35c5d761c04c8151336a9daa842029fd2
Rafiul-Hubar <rafiul.faisal@swordfishtech.co.uk>
2025-04-28 17:44:05 +0600

    swipable list test

--- src/screens/auth/billScreen.tsx
+++ src/screens/auth/billScreen.tsx
@@ -1,15 +1,47 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-const billScreen = () => {
+export const BillScreen = () => {
+  const opacity = useSharedValue(1);
+  const scale = useSharedValue(1);
+  const [a, setA] = useState(false);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    transform: [{scale: scale.value}],
+  }));
+
+  const shrinkAndFade = () => {
+    if (a) {
+      opacity.value = withTiming(1, {duration: 500});
+      scale.value = withTiming(1, {duration: 500});
+    } else {
+      opacity.value = withTiming(0.5, {duration: 1000});
+      scale.value = withTiming(0.5, {duration: 1000});
+    }
+    setA(!a);
+  };
+
   return (
     <SafeAreaView style={{flex: 1, padding: 16}}>
       <View>
         <Text>billScreen</Text>
+        <Animated.View
+          style={[
+            {width: 100, height: 100, backgroundColor: 'red'},
+            animatedStyle,
+          ]}
+        />
+        <Button title="Shrink and Fade" onPress={shrinkAndFade} />
       </View>
     </SafeAreaView>
   );
 };
 
-export default billScreen;
+export default BillScreen;
--- src/screens/auth/profileScreen.tsx
+++ src/screens/auth/profileScreen.tsx
@@ -1,17 +1,98 @@
-import {Text, View} from 'react-native';
-import React, {Component} from 'react';
-import {SafeAreaView} from 'react-native-safe-area-context';
+/* eslint-disable react-hooks/rules-of-hooks */
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 
-export class profileScreen extends Component {
-  render() {
-    return (
-      <SafeAreaView style={{flex: 1, padding: 16}}>
-        <View>
-          <Text>profileScreen</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
+const SwipeableItem = ({
+  item,
+  onDelete,
+}: {
+  item: ItemType;
+  onDelete: (id: number) => void;
+}) => {
+  const translateX = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: translateX.value}],
+  }));
 
-export default profileScreen;
+  // gesture handler to handle swipe
+  const penGesture = Gesture.Pan()
+    .onUpdate(e => {
+      if (e.translationX < 0) {
+        translateX.value = e.translationX;
+      }
+    })
+    .onEnd(() => {
+      if (translateX.value < -200) {
+        translateX.value = withTiming(500);
+        runOnJS(onDelete)(item.id);
+      } else {
+        translateX.value = withTiming(0);
+      }
+    });
+
+  return (
+    <GestureDetector gesture={penGesture}>
+      <Animated.View style={[styles.item, animatedStyle]}>
+        <Text style={styles.itemText}>{item.name}</Text>
+      </Animated.View>
+    </GestureDetector>
+  );
+};
+
+type ItemType = {
+  id: number;
+  name: string;
+};
+
+export const ProfileScreen = () => {
+  const [items, setItems] = useState<ItemType[]>([
+    {id: 1, name: 'Item 1'},
+    {id: 2, name: 'Item 2'},
+    {id: 3, name: 'Item 3'},
+    {id: 4, name: 'Item 4'},
+  ]);
+
+  const handleDelete = (id: number) => {
+    setItems(currentItems => currentItems.filter(item => item.id !== id));
+  };
+
+  const renderItem = ({item}: {item: ItemType}) => {
+    return <SwipeableItem item={item} onDelete={handleDelete} />;
+  };
+  return (
+    <View style={styles.container}>
+      <Text>profileScreen</Text>
+      <FlatList
+        data={items}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  item: {
+    backgroundColor: 'skyblue',
+    padding: 20,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  itemText: {
+    fontSize: 18,
+  },
+});
+
+export default ProfileScreen;
