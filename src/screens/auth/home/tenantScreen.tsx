b244f7ed2fc561ee7f795ff99ffa9b0a764f2fd8
Rafiul007 <rafiul.faisal@gmail.com>
2025-06-14 04:26:00 +0600

    TenantCard component and added to screen

--- src/screens/auth/home/tenantScreen.tsx
+++ src/screens/auth/home/tenantScreen.tsx
@@ -1,14 +1,24 @@
-import {Text, View} from 'react-native';
+// screens/auth/tenantScreen.tsx
 import React from 'react';
+import {ScrollView} from 'react-native';
+import TenantInfoCard from '../../../components/Molucules/TenantInfoCard';
+
+const tenantData = {
+  fullName: 'Rafiul Islam',
+  mobileNumber: '+8801770001234',
+  email: 'rafi@example.com',
+  nidNumber: '1999123456789',
+  rentAmount: '15000',
+  rentStartDate: '2024-01-01',
+  address: 'Flat 2B, NST Tower, Mirpur, Dhaka',
+};
 
 const TenantScreen = () => {
   return (
-    <View>
-      <Text>tenantScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={{padding: 16}}>
+      <TenantInfoCard data={tenantData} />
+    </ScrollView>
   );
 };
 
 export default TenantScreen;
-
-// const styles = StyleSheet.create({});
